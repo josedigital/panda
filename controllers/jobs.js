80537499294bff39c5b44011662abc9740df0048
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-11-19 21:14:31 -0600

    add /jobs view

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var data = require('../models/data.js');
 
 router.get('/jobs', function(req, res, next) {
-  res.render('');
+  res.render('jobs');
 });
 
 router.get('/jobs/:provider', function(req, res, next) {
