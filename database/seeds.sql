2abdf9dc9dc6f0215c91c08e01a0f9f16edda998
George Ramirez <georgeramirez@georges-mbp.gateway.pace.com>
2016-11-20 14:23:59 -0600

    the db schema and seeds added to create locally

---
+++ database/seeds.sql
@@ -0,0 +1,30 @@
+
+INSERT INTO `resource_type` (`id`, `type`)
+VALUES
+	(1,'VIDEO'),
+	(2,'PDF'),
+	(3,'LINK');
+
+# ------------------------------------------------------------
+
+INSERT INTO `technology` (`id`, `tech`, `description`)
+VALUES
+	(1,'HTML',NULL),
+	(2,'CSS',NULL),
+	(3,'JAVASCRIPT',NULL),
+	(4,'JQUERY',NULL),
+	(5,'API',NULL),
+	(6,'NODE.JS',NULL),
+	(7,'NPM',NULL),
+	(8,'mySQL',NULL),
+	(9,'SEQUELIZE',NULL),
+	(10,'EXPRESS',NULL),
+	(11,'mongoDB',NULL),
+	(12,'REACT',NULL),
+	(13,'HANDLEBARS',NULL),
+	(14,'GITHUB',NULL),
+	(15,'HEROKU',NULL),
+	(16,'SECURITY',NULL),
+	(17,'DEBUG',NULL),
+	(18,'TESTING',NULL),
+	(19,'TOOLS',NULL);
