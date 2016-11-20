1f2388ed48edf2ff55d2633a2702d4989045ff6b
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-11-19 22:26:09 -0600

    test render dice jobs to jobs view

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request-promise');
 
 router.get('/jobs', function(req, res, next) {
-  res.render('jobs');
+  request({
+    uri: 'http://service.dice.com/api/rest/jobsearch/v1/simple.json',
+    qs: {
+      text: 'javascript', // req.params.city,
+      city: 'Austin'
+         // Use your accuweather API key here
+    },
+    json: true
+  })
+    .then( function (data) {
+      res.render('jobs', data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+  
 });
 
 router.get('/jobs/:provider', function(req, res, next) {
