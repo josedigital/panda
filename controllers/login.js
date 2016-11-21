4523c0e9c604e7ab878573b39ddd7a5ffee48a26
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-11-20 21:55:38 -0600

    add github api package

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var connectLogin = require('connect-ensure-login');
 
 
 // Login
@@ -16,13 +15,8 @@ router.get('/login/github', passport.authenticate('github'));
 // login/github/return
 router.get('/login/github/return', passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
-    res.redirect('/');
+    res.redirect('/profile');
 });
 
-
-router.get('/profile', connectLogin.ensureLoggedIn(), function(req, res){
-  req.user.img = req.user.photos[0].value;
-  res.render('profile', { user: req.user });
-});
 
 module.exports = router;
--- controllers/profile.js
+++ controllers/profile.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var connectLogin = require('connect-ensure-login');
+var request = require('request-promise');
 
-router.get('/profile', function(req, res, next) {
-  res.send('Hello world!');
+router.get('/profile', connectLogin.ensureLoggedIn(), function(req, res){
+  req.user.img = req.user.photos[0].value;
+  apiURI = 'https://api.github.com/users/' + req.user.username + 'repos';
+  request({
+    uri: apiURI,
+    json: true
+  })
+    .then( function (data) {
+      console.log(queryString);
+      res.render('profile', { user: req.user, data });
+    })
+    .catch( function (error) {
+      console.log(error);
+      res.json(error);
+    });
+  
 });
+
+  
+
 
 router.get('/profile/new', function(req, res, next) {
   res.render('');
