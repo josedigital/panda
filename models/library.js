b65d05ebf7576478297b4f5ac274eea133060d4b
Vincent Nguyen <vnguyen2@gmail.com>
2016-11-21 21:54:12 -0600

    added timestamps false to avoid adding seed conflicts

--- models/library.js
+++ models/library.js
@@ -2,7 +2,10 @@
 module.exports = function(sequelize, DataTypes) {
   var library = sequelize.define('library', {
     resource: DataTypes.STRING
-  }, {
+  },{
+      timestamps: false
+    },
+  {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
