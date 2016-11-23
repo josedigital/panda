bfd6d214522efdeaeeef8e1d0058ba545f1fcfdf
George Ramirez <georgeramirez@Georges-MacBook-Pro.local>
2016-11-22 18:38:28 -0600

    fixed seeds we can use sqlpro to insert

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,30 +1,34 @@
 
-INSERT INTO `resource_types` (`id`, `type`)
+INSERT INTO `resource_types` (`id`, `type`,`createdAt`, `updatedAt`)
 VALUES
-	(1,'VIDEO'),
-	(2,'PDF'),
-	(3,'LINK');
+	(1,'VIDEO', '20161120', '20161120'),
+	(2,'PDF', '20161120', '20161120'),
+	(3,'LINK', '20161120', '20161120');
 
 # ------------------------------------------------------------
 
-INSERT INTO `technologies` (`id`, `tech`, `description`)
+-- INSERT INTO `resource_types` (`id`, `type`, `createdAt`, `updatedAt`)
+-- VALUES
+-- 	(1, 'VIDEO', '20161120', '20161120');
+
+INSERT INTO `technologies` (`id`, `tech`, `description`, `createdAt`, `updatedAt`)
 VALUES
-	(1,'HTML',NULL),
-	(2,'CSS',NULL),
-	(3,'JAVASCRIPT',NULL),
-	(4,'JQUERY',NULL),
-	(5,'API',NULL),
-	(6,'NODE.JS',NULL),
-	(7,'NPM',NULL),
-	(8,'mySQL',NULL),
-	(9,'SEQUELIZE',NULL),
-	(10,'EXPRESS',NULL),
-	(11,'mongoDB',NULL),
-	(12,'REACT',NULL),
-	(13,'HANDLEBARS',NULL),
-	(14,'GITHUB',NULL),
-	(15,'HEROKU',NULL),
-	(16,'SECURITY',NULL),
-	(17,'DEBUG',NULL),
-	(18,'TESTING',NULL),
-	(19,'TOOLS',NULL);
+	(1,'HTML',NULL, '20161120', '20161120'),
+	(2,'CSS',NULL, '20161120', '20161120'),
+	(3,'JAVASCRIPT',NULL, '20161120', '20161120'),
+	(4,'JQUERY',NULL, '20161120', '20161120'),
+	(5,'API',NULL, '20161120', '20161120'),
+	(6,'NODE.JS',NULL, '20161120', '20161120'),
+	(7,'NPM',NULL, '20161120', '20161120'),
+	(8,'mySQL',NULL, '20161120', '20161120'),
+	(9,'SEQUELIZE',NULL, '20161120', '20161120'),
+	(10,'EXPRESS',NULL, '20161120', '20161120'),
+	(11,'mongoDB',NULL, '20161120', '20161120'),
+	(12,'REACT',NULL, '20161120', '20161120'),
+	(13,'HANDLEBARS',NULL, '20161120', '20161120'),
+	(14,'GITHUB',NULL, '20161120', '20161120'),
+	(15,'HEROKU',NULL, '20161120', '20161120'),
+	(16,'SECURITY',NULL, '20161120', '20161120'),
+	(17,'DEBUG',NULL, '20161120', '20161120'),
+	(18,'TESTING',NULL, '20161120', '20161120'),
+	(19,'TOOLS',NULL, '20161120', '20161120');
