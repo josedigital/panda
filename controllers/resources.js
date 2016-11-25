d63a67ac7274d540641fa5166873765573f59a4b
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-11-24 22:15:06 -0600

    add meetup api with tech routing

--- controllers/resources.js
+++ controllers/resources.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request-promise');
 
-router.get('/resources', function(req, res, next) {
-  res.render('');
+router.get('/resources/meetups/:tech', function(req, res, next) {
+
+  request({
+    uri: 'https://api.meetup.com/find/groups',
+    qs: {
+      'key': 'xxxxxxxxxxxxxxx',
+      'sign': true,
+      'photo-host': 'public',
+      'zip': 78759,
+      'country': 'usa',
+      'filter': 'all',
+      'location': 'austin',
+      'text': req.params.tech,
+      'radius': 50,
+      'page': 20
+    },
+    json: true
+  })
+    .then( function (data) {
+      res.render('resources', data);
+    })
+    .catch( function (error) {
+      console.log(error);
+      res.json(error);
+    });
+
 });
 
 router.get('/resources/:tech', function(req, res, next) {
