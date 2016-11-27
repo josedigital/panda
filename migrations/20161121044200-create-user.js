1b51b8f93b809dd8da89b8837a752333b5f9c4c7
Vincent Nguyen <vnguyen2@gmail.com>
2016-11-27 10:39:42 -0600

    updated user table columns

--- migrations/20161121044200-create-user.js
+++ migrations/20161121044200-create-user.js
@@ -20,29 +20,8 @@ module.exports = {
       git_link: {
         type: Sequelize.STRING
       },
-      technologies: {
-        type: Sequelize.STRING
-      },
       main_text: {
         type: Sequelize.TEXT
-      },
-      git_repo1: {
-        type: Sequelize.STRING
-      },
-      git_text1: {
-        type: Sequelize.STRING
-      },
-      git_repo2: {
-        type: Sequelize.STRING
-      },
-      git_text2: {
-        type: Sequelize.STRING
-      },
-      git_repo3: {
-        type: Sequelize.STRING
-      },
-      git_text3: {
-        type: Sequelize.STRING
       },
       avitar_link: {
         type: Sequelize.STRING
